6a3df7976f1484cf5aa9022d76cf7f4058e97dfe
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-01 15:06:41 +0530

    HIIIIIIIII

---
+++ my programming/html css webpages/JavaScript/OnClickPopup.js
@@ -0,0 +1,14 @@
+function warning() {
+  if (document.getElementById("email").value == "")
+    document.getElementById("pass").value == "";
+  document.getElementById("email").value = "Enter Email";
+  document.getElementById("pass").style.borderColor = "red";
+
+  {
+    alert("PLEASE FILL THE FORM");
+    return false;
+  }
+}
+function image() {
+  document.getElementById("form").style.backgroundImage = "url('logo.png')";
+}
