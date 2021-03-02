81d3bca035c6e0710985e39380a8d921f369cefc
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-02 11:42:31 +0530

    hiiiiiiiiiiiiii

--- my programming/html css webpages/JavaScript/OnClickPopup.js
+++ my programming/html css webpages/JavaScript/OnClickPopup.js
@@ -1,15 +1,13 @@
 function warning() {
   if (document.getElementById("email").value == "")
     document.getElementById("pass").value == "";
-  document.getElementById("email").value = "Enter Email";
+  document.getElementById("email").style.borderColor = "red";
   document.getElementById("pass").style.borderColor = "red";
 
-    {
-        alert("PLEASE FILL THE FORM");
-        return false;
-    }
-        
-}
+  {
+    alert("PLEASE FILL THE FORM");
+    return false;
+  }
 }
 function image() {
   document.getElementById("form").style.backgroundImage = "url('logo.png')";
