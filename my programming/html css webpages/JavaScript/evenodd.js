e789108197141f0ac22791d8748cfc2a7ce0f846
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-03-10 15:10:59 +0530

    hiiiiiiiii

---
+++ my programming/html css webpages/JavaScript/evenodd.js
@@ -0,0 +1,13 @@
+function msg() {
+  n = Number(document.getElementById("input").value);
+  if (n % 2 == 0) {
+    // alert("number is even");
+    document.getElementById("demo").innerHTML = "number is even";
+  } else {
+    // alert("number is odd");
+    document.getElementById("demo").innerHTML = "number is odd";
+  }
+}
+function msg2() {
+
+}
