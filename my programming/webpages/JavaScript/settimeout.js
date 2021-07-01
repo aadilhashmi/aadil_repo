e3bc752b68e1cbedef2342e74f606643a3d248bb
aadilhashmi <78012474+aadilhashmi@users.noreply.github.com>
2021-07-01 16:03:12 +0530

    hiiiiii

--- my programming/webpages/JavaScript/settimeout.js
+++ my programming/webpages/JavaScript/settimeout.js
@@ -1,7 +1,20 @@
-// var i = 1;
-function time() {
-  for (var i = 1; i >= 5; i++);
-  setTimeout(() => {
-    console.log("hiiii", i);
-  }, i * 1000);
-}
+// // var i = 1;
+// const time = () => {
+//   for (let i = 1; i <= 5; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+//   console.log("hello js");
+// };
+// time();
+
+const setUser = () => {
+  for (let i = 0; i < Math.random() * 100; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 500);
+  }
+  console.log("print array elements");
+};
+setUser();
